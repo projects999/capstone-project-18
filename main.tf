c90861d3f817ff5678124d2f9d7c5a96bcd7b990
clouduser <mainktyagis06@gmail.com>
2026-06-25 14:04:59 +0530

    commit on feature-update branch

--- main.tf
+++ main.tf
@@ -8,5 +8,5 @@ terraform {
 provider "local" {}
 resource "local_file" "gitops_demo" {
     filename = "gitops.txt"
-    content = "GitOps deployment successful"
+    content = "Updated through GitOps"
 }
